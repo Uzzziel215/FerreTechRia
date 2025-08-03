255f9bcb1ad0492740ee918ec6bce57b89e1b5ea
Uzziel <cucr1605@gmail.com>
2025-08-03 02:58:59 -0600

    fix: Añadir importaciones faltantes en la entidad Venta

--- backend/src/main/java/com/ferreteria/model/Venta.java
+++ backend/src/main/java/com/ferreteria/model/Venta.java
@@ -1,6 +1,18 @@
 package com.ferreteria.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
