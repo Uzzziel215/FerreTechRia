6fa669347a4c18500f71850838fff3d55e91c1cd
Uzziel <cucr1605@gmail.com>
2025-08-02 19:08:12 -0600

    feat: Use environment variable for API URL

--- services/apiConfig.js
+++ services/apiConfig.js
@@ -3,7 +3,7 @@ import { toast } from "react-hot-toast"
 
 // Configuración base de axios
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
